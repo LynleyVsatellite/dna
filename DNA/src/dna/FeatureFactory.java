8e8a45791829e2836456579a9e98652821bb13b1
Rakan Dirbas <rakan.dirbas@gmail.com>
2015-04-23 13:30:32 +0200

    Replaced the POS and NER features with the CoreNLP feature

--- DNA/src/dna/FeatureFactory.java
+++ DNA/src/dna/FeatureFactory.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dna.features.AllCapitalizedDNAFeature;
+import dna.features.CoreNLPDNAFeature;
 import dna.features.HasCapitalLetterDNAFeature;
 import dna.features.HasWeirdCharDNAFeature;
 import dna.features.IsANumberDNAFeature;
-import dna.features.NERDNAFeature;
 import dna.features.NGramDNAFeature;
 import dna.features.NumberOfCharsDNAFeature;
-import dna.features.POSDNAFeature;
 import dna.features.WordDNAFeature;
 
 /**
@@ -28,15 +27,16 @@ public class FeatureFactory {
 		this.features = new ArrayList<DNAFeature>();
 		this.totalNumberOfFeatures = 0;
 
-		features.add( new AllCapitalizedDNAFeature() );
 		features.add( new HasCapitalLetterDNAFeature() );
+		features.add( new CoreNLPDNAFeature() );
 		features.add( new HasWeirdCharDNAFeature() );
+		features.add( new AllCapitalizedDNAFeature() );
 		features.add( new IsANumberDNAFeature() );
-		features.add( new NERDNAFeature() );
-		features.add( new NGramDNAFeature(3) );
+//		features.add( new NERDNAFeature() );
 		features.add( new NumberOfCharsDNAFeature() );
-		features.add( new POSDNAFeature() );
+//		features.add( new POSDNAFeature() );
 		features.add( new WordDNAFeature() );
+		features.add( new NGramDNAFeature(3) );
 		
 		for ( DNAFeature f : this.features ) {
 			this.totalNumberOfFeatures += f.numberOfFeatures();
