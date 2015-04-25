db1807601b359e1b77e2c4ba03582a72f2a6e838
Rakan Dirbas <rakan.dirbas@gmail.com>
2015-04-25 17:08:02 +0200

    The FeatureFactory now contains a field that corresponds to the ids of the features

--- DNA/src/dna/features/FeatureFactory.java
+++ DNA/src/dna/features/FeatureFactory.java
@@ -1,6 +1,9 @@
 package dna.features;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import dna.DNAFeature;
 import dna.DNAToken;
@@ -15,20 +18,31 @@ public class FeatureFactory {
 	private int totalNumberOfFeatures;
 	private List<DNAFeature> features;
 	private Vocabulary vocab;
+	private List<String> featuresNames;
 	
-	public FeatureFactory(List<DNAToken> tokens, List<DNAFeature> features) {
+	public FeatureFactory(List<DNAToken> tokens, List<DNAFeature> features, 
+			Map<String, Set<Integer>> trainTestValDocsIds) {
 		this.tokens = tokens;
 		this.features = features;
 		this.totalNumberOfFeatures = 0;
-		this.vocab = new Vocabulary(tokens);
+		this.vocab = new Vocabulary(tokens, trainTestValDocsIds);
+		this.featuresNames = new ArrayList<String>();
+		
 		for ( DNAFeature f : features ) {
 			if ( f instanceof VocabularyDependent )
 				((VocabularyDependent) f).setVocab(vocab);
 		}
 		
-		for ( DNAFeature f : this.features ) {
+		for ( int j = 0; j < features.size(); j++ ) {
+			DNAFeature f = features.get(j);
+			for( int i = 0; i < f.numberOfFeatures(); i++ ) {
+				featuresNames.add( j + "_" + i );
+			}
 			this.totalNumberOfFeatures += f.numberOfFeatures();
 		}
+		
+		if( featuresNames.size() != totalNumberOfFeatures)
+			throw new RuntimeException("The number of features names is not the same as the number of features!");
 		
 	}
 	
@@ -54,6 +68,10 @@ public class FeatureFactory {
 	
 	public int getTotalNumberOfFeatures() {
 		return totalNumberOfFeatures;
+	}
+
+	public List<String> getFeaturesNames() {
+		return featuresNames;
 	}
 	
 
