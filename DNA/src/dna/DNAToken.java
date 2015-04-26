3debaa03e02536056239a6242829d630cf2e678a
Rakan Dirbas <rakan.dirbas@gmail.com>
2015-04-26 09:09:51 +0200

    Introduced another variable called index that holds the position of the token within a document and modified the old id to become the id of the token within a collection.

--- DNA/src/dna/DNAToken.java
+++ DNA/src/dna/DNAToken.java
@@ -8,12 +8,17 @@ public class DNAToken {
 	private String text;
 	private int start_position;
 	private int end_position;
-	private String label;//either a positive class token or a negative class token
+	//either a positive class token or a negative class token
+	private String label;
 	private SparseVector features;
-	private int id;
-	//The ID of the document (from the single dna database file) that this token belongs to.
+	//the position of the token within a document (1st token or 2nd token ... etc)
+	private int index;
+	//the id of the token in the document collection.
+	private int id; 
+	//The ID of the document (per a single dna database file) that this token belongs to.
 	private int docId;
 	//This id is used to identify documents when using many dna files, i.e. to avoid duplicates.
+	//in other words this is to identify the document from the entire documents collection.
 	private int internalDocId;
 	
 	public DNAToken() {
@@ -63,6 +68,15 @@ public class DNAToken {
 		this.id = id;
 	}
 	
+	
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
 	/**
 	 * @return The ID of the document that this token belongs to.
 	 */
